8017f12c2e0fa4fbfaf278d0730efc0f8934ce38
dinasv <dinasv@post.bgu.ac.il>
2021-04-26 15:49:54 +0300

    Style change for docstring, remove unused function

--- Code/train_and_predict.py
+++ Code/train_and_predict.py
@@ -21,7 +21,7 @@ AUPR = 'AUPR'
 
 
 def predict(X_test, model):
-    '''Predicts labels of X_test with a specific model
+    """Predicts labels of X_test with a specific model
 
 
     Parameters:
@@ -31,7 +31,7 @@ def predict(X_test, model):
     Returns:
     predictions - final predictions (0 - NHP, 1- HP)
     predictions_probs - probability estimates
-    '''
+    """
 
     predictions = model.predict(X_test)
     predictions_probs = model.predict_proba(X_test)[:, 1]
@@ -41,7 +41,7 @@ def predict(X_test, model):
 
 
 def predict_and_print_results(X_test, y_test, model):
-    '''Predicts labels of X_test with a specific model and returns
+    """Predicts labels of X_test with a specific model and returns
        evaluation results of the model accuracy
 
     Parameters:
@@ -51,7 +51,7 @@ def predict_and_print_results(X_test, y_test, model):
 
     Returns:
     evaluation_results -  dictionary of the sensitivity, specificity, f1_macro, aupr_auc and roc_auc scores
-    '''
+    """
 
 
     predictions, predictions_probs = predict(X_test, model)
@@ -61,9 +61,8 @@ def predict_and_print_results(X_test, y_test, model):
     return evaluation_results
 
 
-
 def train_and_predict(X_train, y_train, X_test, y_test, features, random_state=0):
-    '''Trains a new model, predicts test genoomes and prints accuracy evaluation results
+    """Trains a new model, predicts test genoomes and prints accuracy evaluation results
 
     Parameters:
     X_train - dataframe represents the train genomes feature vectors
@@ -75,7 +74,7 @@ def train_and_predict(X_train, y_train, X_test, y_test, features, random_state=0
     Returns:
     fs_rs_model - resulting model
     evaluation_results -  dictionary of the sensitivity, specificity, f1_macro, aupr_auc and roc_auc scores
-    '''
+    """
 
     rs_model = Pipeline(steps=[('vectorize', CountVectorizer(lowercase=False, binary=True, vocabulary=features)),
                                ('rf', RandomForestClassifier(random_state=random_state))])
@@ -89,7 +88,7 @@ def train_and_predict(X_train, y_train, X_test, y_test, features, random_state=0
 
 
 def calc_confusion_matrix_indexes(predictions, y_test):
-    '''calculates confusion matrix
+    """calculates confusion matrix
 
     Parameters:
     predictions - predicted labels
@@ -101,7 +100,7 @@ def calc_confusion_matrix_indexes(predictions, y_test):
         true_positive -list of positive genomes with positive prediction value
         false_positive -  list of negative genomes with positive prediction value
         false_negative - list of positive genomes with negative prediction value
-    '''
+    """
 
     true_negative = [i for i in range(len(predictions)) if (predictions[i] == y_test[i] == 0)]
     true_positive = [i for i in range(len(predictions)) if (predictions[i] == y_test[i] == 1)]
@@ -111,25 +110,23 @@ def calc_confusion_matrix_indexes(predictions, y_test):
     return true_negative, true_positive, false_positive, false_negative
 
 
-
 def calc_confusion_matrix(predictions, y_test):
-    '''calculates confusion matrix
+    """calculates confusion matrix
 
     Parameters:
     predictions - predicted labels
     y_test - dataframe represents the train genomes true labels
 
     Returns: true_negative, true_positive, false_positive and false_negative values
-    '''
+    """
 
     true_negative, true_positive, false_positive, false_negative = calc_confusion_matrix_indexes(predictions, y_test)
 
     return len(true_negative), len(true_positive), len(false_positive), len(false_negative)
 
 
-
 def calculate_results(predictions, predictions_probs, y_test):
-    '''Calculates accuracy results
+    """Calculates accuracy results
 
     Parameters:
     predictions - predictions (list of genomes predictions as 1 or 0)
@@ -139,7 +136,7 @@ def calculate_results(predictions, predictions_probs, y_test):
     Returns:
     sensitivity, specificity, f1_macro, aupr_auc and roc_auc results
 
-    '''
+    """
 
     true_negative, true_positive, false_positive, false_negative = calc_confusion_matrix(predictions, y_test)
     print('false_positive: ' + str(false_positive) + ',total NHPs: ' + str(true_negative + false_positive))
@@ -163,16 +160,14 @@ def calculate_results(predictions, predictions_probs, y_test):
     return evaluation_results
 
 
-
 def print_train_results(evaluation_results):
-    '''Print BACC, sensitivity, specificity, AUPR and AUROC results
+    """Print BACC, sensitivity, specificity, AUPR and AUROC results
 
     Parameters:
     evaluation_results - dictionary of the sensitivity, specificity, f1_macro, aupr_auc and roc_auc scores
 
     Returns:
-    '''
-
+    """
 
     ROUND = 2
     bacc = round(evaluation_results[BACC], ROUND)
@@ -189,14 +184,14 @@ def print_train_results(evaluation_results):
 
 
 def dendogram(X, corr_linkage):
-    '''Draws dendogram
+    """Draws dendogram
 
     Parameters:
     X - dataframe represents the train genomes feature vectors
     corr_linkage - the hierarchical clustering encoded as a linkage matrix
 
     Returns:
-    '''
+    """
 
     fig, ax = plt.subplots(figsize=(12, 8))
 
@@ -208,41 +203,4 @@ def dendogram(X, corr_linkage):
     fig.tight_layout()
     plt.show()
 
-
-############# Predict pathogenicity labels with existing model#########
-# TODO: Modify function
-def predict_with_existing_model(classifier, classifiers_features, input_genumes_file, output_files_str):
-    ''' Predicts the pathogenicity label of genomes with existing classification model,
-     and returns dataframe of the results
-
-    Parameters:
-    classifier - existing classification model
-    classifiers_features - dioctionary represents the feature set of the classifier
-    input_genumes_file - file of input genomes (each genomes is represented as a sequence of PATRIC Protein Global
-                        Families (PGFams) identifiers)
-    output_files_str - string for   output files naming
-
-    Returns:
-    results df - dataframe that contains the predictions and the predicted probabilities for the input genomes
-    '''
-    with open(classifiers_features) as f:
-        pgfams_dict = json.load(f)
-
-    output_vec_file = output_files_str + '_vecs.json'
-    util.create_features_vec(pgfams_dict, [input_genumes_file], output_vec_file)
-    X_genomes = util.read_pgfams_data_json(output_vec_file, classifiers_features)
-    predictions, predictions_probs = predict(X_genomes, classifier)
-
-    pathogenicity_preds = []
-    for i in predictions:
-        if i == 0:
-            pathogenicity_preds.append('NHP')
-        else:
-            pathogenicity_preds.append('HP')
-
-    d = {'Genome ID': X_genomes.index ,'prediction': pathogenicity_preds, 'predictions_prob': predictions_probs}
-    output_df = pd.DataFrame(data=d)
-    output_df = output_df.set_index('Genome ID')
-
-    return output_df
 
